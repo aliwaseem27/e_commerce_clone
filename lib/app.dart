9f3c84c232b51bf8da90a790e1997c9d484922c8
aliwaseem27 <ali.waseem27@gmail.com>
2024-06-10 23:36:05 +0300

    add: forget password screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:e_commerce_clone/presentation/screens/forget_password/forget_password_screen.dart';
 import 'package:e_commerce_clone/presentation/screens/sign_in/sign_in_screen.dart';
 import 'package:e_commerce_clone/presentation/screens/sign_up/sign_up_screen.dart';
 import 'package:e_commerce_clone/utils/themes/theme.dart';
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       builder: DevicePreview.appBuilder,
       title: 'Flutter Demo',
       theme: AppTheme.lightTheme,
-      home: SignInScreen(),
+      home: ForgetPasswordScreen(),
     );
   }
 }
---
+++ lib/presentation/screens/forget_password/widgets/forget_password_form.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../../../../utils/constants/app_sizes.dart';
+import '../../../../utils/constants/app_strings.dart';
+
+class ForgetPasswordForm extends StatelessWidget {
+  const ForgetPasswordForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          TextFormField(decoration: InputDecoration(labelText: AppStrings.emailAddress)),
+          SizedBox(height: AppSizes.spaceBtwInputFields),
+
+          // Sign up button
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                AppStrings.sendCode,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
