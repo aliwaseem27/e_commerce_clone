d189df3984005f6c2babccaacb9447c4206cdda3
aliwaseem27 <ali.waseem27@gmail.com>
2024-06-16 18:29:15 +0300

    add: Categories Home Screen

--- lib/presentation/screens/store/home_screen.dart
+++ lib/presentation/screens/store/home_screen.dart
@@ -1,9 +1,12 @@
 import 'package:auto_route/annotations.dart';
 import 'package:e_commerce_clone/utils/constants/app_colors.dart';
 import 'package:e_commerce_clone/utils/constants/app_sizes.dart';
+import 'package:e_commerce_clone/utils/constants/image_strings.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import '../../common/widgets/section_title.dart';
+import 'widgets/category_vertical_widget.dart';
 import 'widgets/home_screen_header.dart';
 import 'widgets/home_search_bar.dart';
 
@@ -13,6 +16,17 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<Map<String, String>> categories = [
+      {"title": "Sports", "image": ImageStrings.sportIcon},
+      {"title": "Clothing", "image": ImageStrings.clothIcon},
+      {"title": "Shoes", "image": ImageStrings.shoeIcon},
+      {"title": "Cosmetics", "image": ImageStrings.cosmeticsIcon},
+      {"title": "Animals", "image": ImageStrings.animalIcon},
+      {"title": "Toys", "image": ImageStrings.toyIcon},
+      {"title": "Furniture", "image": ImageStrings.furnitureIcon},
+      {"title": "Jewelery", "image": ImageStrings.jeweleryIcon},
+      {"title": "Electronics", "image": ImageStrings.electronicsIcon},
+    ];
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.all(AppSizes.defaultSpace),
@@ -27,10 +41,38 @@ class HomeScreen extends StatelessWidget {
               // Search Bar
               HomeSearchBar(),
 
-              // Promo Banners
-              Container(),
+              SizedBox(height: AppSizes.spaceBtwItems),
 
-              // Categories
+              // Promo Banners
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 150,
+                color: AppColors.primaryColor,
+              ),
+
+              SizedBox(height: AppSizes.spaceBtwItems),
+
+              // Categories Header
+              SectionTitle(
+                title: "Category",
+                onPressed: () {},
+              ),
+
+              // Categories List
+              SizedBox(
+                height: 100,
+                child: ListView.builder(
+                  itemCount: categories.length,
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return CategoryVerticalWidget(
+                      title: categories[index].values.first,
+                      image: categories[index].values.last,
+                    );
+                  },
+                ),
+              ),
 
               // Products for you
 
--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -1,3 +1,14 @@
 class ImageStrings {
   static const String profileImage = "assets/images/me.jpg";
+
+  // Categories
+  static const String sportIcon = "assets/images/categories/icons8-bowling-64.png";
+  static const String clothIcon = "assets/images/categories/icons8-tailors-dummy-64.png";
+  static const String shoeIcon = "assets/images/categories/icons8-shoes-64.png";
+  static const String cosmeticsIcon = "assets/images/categories/icons8-cosmetics-64.png";
+  static const String animalIcon = "assets/images/categories/icons8-dog-heart-64.png";
+  static const String toyIcon = "assets/images/categories/icons8-wooden-toy-car-50.png";
+  static const String furnitureIcon = "assets/images/categories/icons8-dining-chair-64.png";
+  static const String jeweleryIcon = "assets/images/categories/icons8-sparkling-diamond-64.png";
+  static const String electronicsIcon = "assets/images/categories/icons8-smartphone-64.png";
 }
