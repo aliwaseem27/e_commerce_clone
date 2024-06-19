c3cc27991f192d1a1039ec1b80085c108a7e171f
aliwaseem27 <ali.waseem27@gmail.com>
2024-06-20 01:05:19 +0300

    finish: Cart Screen

--- lib/presentation/screens/cart/cart_screen.dart
+++ lib/presentation/screens/cart/cart_screen.dart
@@ -1,7 +1,4 @@
-import 'dart:ui';
-
 import 'package:auto_route/annotations.dart';
-import 'package:e_commerce_clone/utils/constants/app_colors.dart';
 import 'package:e_commerce_clone/utils/constants/image_strings.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +6,10 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 
 import '../../../utils/constants/app_sizes.dart';
+import 'widgets/delete_product_slider_action.dart';
+import 'widgets/horizontal_product_card.dart';
+import 'widgets/promo_code_input_field.dart';
+import 'widgets/total_price_group.dart';
 
 @RoutePage()
 class CartScreen extends StatefulWidget {
@@ -67,50 +68,73 @@ class _CartScreenState extends State<CartScreen> {
               bottom: AppSizes.sm),
           child: SlidableAutoCloseBehavior(
             child: SingleChildScrollView(
-              child: ListView.separated(
-                key: UniqueKey(),
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: products.length,
-                itemBuilder: (context, index) {
-                  return Slidable(
+              child: Column(
+                children: [
+                  // Cart Products List
+                  ListView.separated(
                     key: UniqueKey(),
-                    endActionPane: ActionPane(
-                      extentRatio: 0.2,
-                      motion: ScrollMotion(),
-                      dismissible: DismissiblePane(
-                        closeOnCancel: true,
-                        onDismissed: () {
-                          setState(() {
-                            products.removeAt(index);
-                          });
-                        },
-                      ),
-                      dragDismissible: true,
-                      children: [
-                        // SlidableAction(onPressed: noAction, label: "hello"),
-                        DeleteCustomSliderAction(
-                          onTap: () {
-                            Slidable.of(context)?.close();
-                            return ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                content: Text('Delete'),
-                              ),
-                            );
-                          },
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: products.length,
+                    itemBuilder: (context, index) {
+                      return Slidable(
+                        key: UniqueKey(),
+                        endActionPane: ActionPane(
+                          extentRatio: 0.2,
+                          motion: ScrollMotion(),
+                          dismissible: DismissiblePane(
+                            closeOnCancel: true,
+                            onDismissed: () {
+                              setState(() {
+                                products.removeAt(index);
+                              });
+                            },
+                          ),
+                          dragDismissible: true,
+                          children: [
+                            DeleteProductSliderAction(
+                              onTap: () {
+                                Slidable.of(context)?.close();
+                                return ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    content: Text('Delete'),
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
                         ),
-                      ],
+                        child: HorizontalProductCard(
+                          productName: products[index]["name"]!,
+                          productImage: products[index]["image"]!,
+                          productCategory: products[index]["category"]!,
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return SizedBox(height: AppSizes.sm);
+                    },
+                  ),
+
+                  SizedBox(height: AppSizes.spaceBtwSections),
+                  // Promo Code
+                  PromoCodeInputField(),
+
+                  SizedBox(height: AppSizes.spaceBtwSections),
+                  // Total Price
+                  TotalPriceGroup(),
+
+                  SizedBox(height: AppSizes.spaceBtwSections),
+                  // Checkout Button
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("Checkout"),
                     ),
-                    child: HorizontalProductCard(
-                      productName: products[index]["name"]!,
-                      productImage: products[index]["image"]!,
-                      productCategory: products[index]["category"]!,
-                    ),
-                  );
-                },
-                separatorBuilder: (context, index) {
-                  return SizedBox(height: AppSizes.sm);
-                },
+                  ),
+                  SizedBox(height: AppSizes.spaceBtwSections),
+                ],
               ),
             ),
           ),
@@ -119,108 +143,3 @@ class _CartScreenState extends State<CartScreen> {
     );
   }
 }
-
-class DeleteCustomSliderAction extends StatelessWidget {
-  const DeleteCustomSliderAction({
-    super.key,
-    this.onTap,
-  });
-
-  final Function()? onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      flex: 1,
-      child: GestureDetector(
-        onTap: onTap,
-        child: Container(
-          margin: EdgeInsets.only(left: AppSizes.md),
-          height: double.infinity,
-          decoration: BoxDecoration(
-            color: Colors.red.shade50,
-            borderRadius: BorderRadius.circular(AppSizes.borderRadiusLg),
-          ),
-          child: Icon(
-            Icons.delete_forever_rounded,
-            color: Colors.red.shade500,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class HorizontalProductCard extends StatelessWidget {
-  const HorizontalProductCard({
-    super.key,
-    required this.productName,
-    required this.productImage,
-    required this.productCategory,
-  });
-
-  final String productName;
-  final String productImage;
-  final String productCategory;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(AppSizes.md),
-      decoration: BoxDecoration(
-        color: AppColors.secondaryColor,
-        borderRadius: BorderRadius.circular(AppSizes.borderRadiusMd),
-      ),
-      child: Row(
-        children: [
-          // Product Image
-          Expanded(
-            flex: 2,
-            child: AspectRatio(
-              aspectRatio: 1,
-              child: Container(
-                margin: EdgeInsets.only(right: AppSizes.spaceBtwItems),
-                child: Image.asset(productImage),
-              ),
-            ),
-          ),
-
-          // Product Details
-          Expanded(
-            flex: 4,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Product Name
-                Text(
-                  productName,
-                  style: Theme.of(context).textTheme.titleSmall?.copyWith(fontWeight: FontWeight.bold),
-                ),
-
-                // Product Size & quantity
-                Text(
-                  "Qty: 1",
-                  style: Theme.of(context).textTheme.labelSmall?.apply(color: AppColors.neutralColor),
-                ),
-
-                // Product Category
-                Text(
-                  productCategory,
-                  style: Theme.of(context).textTheme.bodySmall?.apply(color: AppColors.neutralColor),
-                ),
-
-                // Product Price
-                Text(
-                  "\$800",
-                  style: Theme.of(context).textTheme.titleLarge?.copyWith(fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-void noAction(BuildContext context) {}
