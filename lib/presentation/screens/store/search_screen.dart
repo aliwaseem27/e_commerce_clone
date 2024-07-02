f04a9d2199744bb36f79d6425589305b734d5371
aliwaseem27 <ali.waseem27@gmail.com>
2024-07-02 23:40:01 +0300

    filler

--- lib/presentation/screens/store/search_screen.dart
+++ lib/presentation/screens/store/search_screen.dart
@@ -5,6 +5,10 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text("Search Screen"),
+      ),
+    );
   }
 }
