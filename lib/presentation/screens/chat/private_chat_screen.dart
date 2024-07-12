28d2131b59b7cae326c517d4e02bb38248f64956
aliwaseem27 <ali.waseem27@gmail.com>
2024-07-12 21:34:38 +0300

    add: Private Chat Screen Refactor

--- lib/presentation/screens/chat/private_chat_screen.dart
+++ lib/presentation/screens/chat/private_chat_screen.dart
@@ -4,6 +4,9 @@ import 'package:e_commerce_clone/utils/constants/app_sizes.dart';
 import 'package:e_commerce_clone/utils/constants/app_strings.dart';
 import 'package:flutter/material.dart';
 
+import 'widgets/chat_bubble.dart';
+import 'widgets/chat_input_field.dart';
+
 @RoutePage()
 class PrivateChatScreen extends StatelessWidget {
   const PrivateChatScreen({super.key});
@@ -11,6 +14,26 @@ class PrivateChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final List<Map<String, dynamic>> messages = [
+      {
+        "senderName": "Liam Peterson",
+        "message": AppStrings.placeholder,
+        "time": "9:39",
+      },
+      {
+        "senderName": "Ali Waseem",
+        "message": AppStrings.placeholder,
+        "time": "9:31",
+      },
+      {
+        "senderName": "Liam Peterson",
+        "message": AppStrings.placeholder,
+        "time": "9:32",
+      },
+      {
+        "senderName": "Ali Waseem",
+        "message": AppStrings.placeholder,
+        "time": "9:34",
+      },
       {
         "senderName": "Liam Peterson",
         "message": AppStrings.placeholder,
@@ -67,51 +90,7 @@ class PrivateChatScreen extends StatelessWidget {
                     shrinkWrap: true,
                     reverse: true,
                     itemBuilder: (context, index) {
-                      return Container(
-                        constraints: BoxConstraints(
-                          maxWidth: MediaQuery.of(context).size.width * 0.8,
-                        ),
-                        padding: EdgeInsets.all(AppSizes.sm),
-                        decoration: BoxDecoration(
-                            color: messages[index]["senderName"] == personName
-                                ? AppColors.secondaryColor
-                                : AppColors.primaryColor,
-                            borderRadius: BorderRadius.circular(AppSizes.borderRadiusLg)),
-                        child: Stack(
-                          children: [
-                            Text(
-                              messages[index]["message"],
-                              style: TextStyle(
-                                color: messages[index]["senderName"] == personName ? Colors.black : Colors.white,
-                              ),
-                            ),
-                            Positioned(
-                              right: 0,
-                              bottom: 0,
-                              child: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  Text(
-                                    messages[index]["time"],
-                                    style: Theme.of(context).textTheme.bodySmall?.apply(color: AppColors.neutralColor),
-                                  ),
-                                  SizedBox(
-                                    width: AppSizes.xs,
-                                  ),
-                                  // Sent Mark
-                                  messages[index]["senderName"] != personName
-                                      ? Icon(
-                                          Icons.done_all,
-                                          size: 16,
-                                          color: AppColors.neutralColor,
-                                        )
-                                      : SizedBox(),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
+                      return ChatBubble(message: messages[index], personName: personName);
                     },
                     separatorBuilder: (context, index) {
                       return SizedBox(height: AppSizes.spaceBtwItems);
@@ -122,45 +101,7 @@ class PrivateChatScreen extends StatelessWidget {
               ),
 
               // Bottom Box
-              Row(
-                children: [
-                  // Text Box
-                  Expanded(
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: "Type A Message",
-                        floatingLabelBehavior: FloatingLabelBehavior.never,
-                        prefixIcon: Icon(
-                          Icons.tag_faces_rounded,
-                          color: AppColors.primaryColor,
-                        ),
-                        suffixIcon: Icon(
-                          Icons.mic,
-                          color: AppColors.primaryColor,
-                        ),
-                      ),
-                    ),
-                  ),
-
-                  SizedBox(
-                    width: AppSizes.spaceBtwItems,
-                  ),
-
-                  // Send Button
-                  Container(
-                    height: 56,
-                    decoration: BoxDecoration(
-                      color: AppColors.primaryColor,
-                      borderRadius: BorderRadius.circular(AppSizes.borderRadiusMd),
-                    ),
-                    child: IconButton(
-                      color: Colors.white,
-                      onPressed: () {},
-                      icon: Icon(Icons.send),
-                    ),
-                  ),
-                ],
-              ),
+              ChatInputField(),
             ],
           ),
         ),
